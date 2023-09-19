5dc829bae9f144701117665b54175545d463a8d4
Treize Konstantin <treize.konstantin@yandex.ru>
2023-09-19 11:28:12 +0600

    added some plugins, changed colorscheme

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,13 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  -- config = function()
+  --   require("todo-comments").setup {}
+  -- end,
+  opts = {},
+  event = "User AstroFile",
+  cmd = { "ShowTodos" },
+  keys = {
+    { "<leader>ST", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope" },
+  },
 }
