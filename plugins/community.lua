9062f70bb2e1ea0144c2d02555d0c63be63bbe74
Treize Konstantin <treize.konstantin@yandex.ru>
2023-09-19 12:11:32 +0600

    added more language support

--- plugins/community.lua
+++ plugins/community.lua
@@ -7,4 +7,7 @@ return {
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.typescript" },
 }
